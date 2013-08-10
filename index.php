849ea4d50ee22201f0ac74fa5e4f16adbed1a8e8
Josh Chaiken <josh@jchaike.com>
2013-08-10 12:42:12 -0400

    Cleanup of the index
    Reorganized contents of index.php to be more user friendly. Comments
    were added to document the process.

--- index.php
+++ index.php
@@ -1,24 +1,12 @@
 <?php
-//$homepage = file_get_contents('firstRun.txt');
 if (!(file_exists("theParameters.php")))
-//if (!($homepage=="false"))
 {
-	//first time setup
+	//theParameters.php does not exist, therefore we must set it up!
 	include('setup.php');
 }
 else
 {
-	//its not the first setup, meaning parameters have been set.
+	//great! everything is good to go. Lets load that Instagram stuff!
 	include('instagram.php');
 }
-
-
-
-
 ?>
-
-<!--
-
-https://api.instagram.com/oauth/authorize/?client_id=39318d8c7d3f430fb27ba26da18fdb38&redirect_uri=http://www.jchaike.com/instagram&response_type=code
-
--->
