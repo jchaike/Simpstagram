1189c2058eeddff4f9268dab04d920cd9ee3f771
Josh Chaiken <josh@jchaike.com>
2013-08-13 12:12:27 -0400

    Setup Wizard Enhancements
    "Windows 95" theme applied to the setup process
    CSS linked to jchaike.com server
    Further setup text improvements.

--- setup.php
+++ setup.php
@@ -82,72 +82,70 @@ echo "<script>location.reload();</script";
 
 <html>
 <head>
-<style>
-.wizard {
-    position: relative;
-    height:350px;
-    width:50%;
-    display:none;
-}
-
-.wizard .buttonnext {
-   position: absolute;
-   bottom: 0;
-   right: 0;
-}
-
-.wizard .buttonback {
-   position: absolute;
-   bottom: 0;
-   right: 75;
-}
-</style>
+<link rel="stylesheet" type="text/css" href="http://www.jchaike.com/instagram/style.css">
 <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 </head>
 <body>
-
 	<div id="step0" class="wizard" style="display:block;">
+	<div id="content">
 	<p>Welcome to the Simpstagram first time setup page!<br><br>
 	The purpose of this script is to help you easily setup and obtain all required credentials to get your Instagram integrated in the simplest way<br>
 	<br>
 	Click "next" to continue.
+	</div>
 	<br><br>
 	<input type="button" class="buttonback" id="disabled" disabled value="<<Back"/><input type="button" class="buttonnext" id="next0" value="Next>>"/>
 	</div>
 	
-	<div id="step1" class="wizard" >1) Visit <a href="http://www.instagram.com/developer">Instagram's Developer Page</a>
+	<div id="step1" class="wizard">
+	<div id="content">
+	<h2>Step 1:</h2>
+	Visit the <a target="_blank" href="http://www.instagram.com/developer">Instagram Developer Page</a><br>
 	<br>
 	<img src="http://www.jchaike.com/instagram/step1.gif"/>
+	<br><br>
+	(opens in new tab)
+	</div>
 	<br><input type="button" class="buttonback" id="back1" value="<<Back"/><input type="button" class="buttonnext" id="next1" value="Next>>"/>
 	</div>
 	
 	<div id="step2" class="wizard" >
-	2) Log in, and click "Manage Clients"
-	<br>
+	<div id="content">
+	<h2> Step 2:</h2>
+	Log in, and click "Manage Clients"
+	<br><br>
 	<img src="http://www.jchaike.com/instagram/step2.gif"/>
+	</div>
 	<br><input type="button" class="buttonback" id="back2" value="<<Back"/><input type="button" class="buttonnext" id="next2" value="Next>>"/>
 	</div>
 	
 	<div id="step3" class="wizard" >
-	3) Click "Register a New Client"
+	<div id="content">
+	<h2>Step 3:</h2>
+	Click "Register a New Client"
 	<br>
 	<img src="http://www.jchaike.com/instagram/step3.gif"/>
+	</div>
 	<br><input type="button" class="buttonback" id="back3" value="<<Back"/><input type="button" class="buttonnext" id="next3" value="Next>>"/>
 	</div>
 	
 	<div id="step4" class="wizard" >
-	4) Create your application<br>
-	<b>IMPORTANT: THIS IS YOUR REDIRECT URI:</b><br>
+	<div id="content">
+	<h2>Step 4:</h2>
+	Create your application<br>
+	<b>IMPORTANT:<br><br>THIS IS YOUR REDIRECT URI:</b><br><br><br>
 	<?php 
 	  $url  = @( $_SERVER["HTTPS"] != 'on' ) ? 'http://'.$_SERVER["SERVER_NAME"] :  'https://'.$_SERVER["SERVER_NAME"];
 	//  $url .= ( $_SERVER["SERVER_PORT"] !== 80 ) ? ":".$_SERVER["SERVER_PORT"] : "";
 	  $url .= $_SERVER["REQUEST_URI"];
-	  print '<h2>'.$url.'</h2>'; 
+	  print '<center><b>'.$url.'</b></center>'; 
 	?>
+	</div>
 	<br><input type="button" class="buttonback" id="back4" value="<<Back"/><input type="button" class="buttonnext" id="next4" value="Next>>"/>
 	</div>
 	
 	<div id="step5" class="wizard" >
+	<div id="content">
 	Fill out the information below:<br>
 
 	<form method="post" action="setup.php" name="parametersform">
@@ -155,17 +153,33 @@ echo "<script>location.reload();</script";
 		Secret Key: <input type="text" name="secretKey"><br>
 		Redirect URI: <input type="text" name="redirectURI" value="<?php echo $url;?>"><br>
 		Username: <input type="text" name="username"><br>
-		Number of Photos to Get (less than 33): <input type="text" name="count"><br>
+		Number of Photos (>33): <input type="text" name="count"><br>
 		Image Type:
 		<select name="imageType">
 			<option>standard_resolution</option>
 			<option>low_resolution</option>
 			<option>thumbnail</option>
 		</select>
+		</div>
 		<br>
-		<input type="button" class="buttonback" id="back5" value="<<Back"/><input class="buttonnext" type="submit" name="send" value="Setup"/>
+		<input type="button" class="buttonback" id="back5" value="<<Back"/><input class="buttonnext" type="submit" name="send" value="Install>"/>
 	</form>
 	</div>
+<div id="footer">
+<div id="content">
+Script created by:<br>
+Josh Chaiken (jchaike)<br>
+<a href="http://www.jchaike.com">site</a> | <a href="http://www.github.com/jchaike">git</a> | <a href="http://www.twitter.com/jchaike">tw</a>
+<br>
+<a href="mailto:josh@jchaike.com">email</a>
+<br>
+<div id="notes">
+Clippy, and setup wizard are (C) Microsoft
+Instagram is (C) Instagram Inc.
+Script created by Josh Chaiken
+</div>
+</div>
+</div>
 </body>
 <script src="http://www.jchaike.com/instagram/wizard.js"></script>
 </html>
