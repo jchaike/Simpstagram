f137a440d5b349555d449ed791d0251c8899f21c
Josh Chaiken <josh@jchaike.com>
2013-08-13 11:15:28 -0400

    Index Warnings Fixed
    Header caused issues with session_start();
    Removed for now, will be placed elsewhere at a later point in time.

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
-<!--
-	Created by jchaike (Josh Chaiken)
-	www.jchaike.com
-	
-	Once everything is setup, feel free to not use this index anymore. 
-	This is simply for ease of use.
---->
 <?php
-session_start();
+//session_start();
 //Checks for theParameters.php. If it exists, include the first time setup file, if not, include instagram.php
 if (!(file_exists("parameters.php")))
 {
