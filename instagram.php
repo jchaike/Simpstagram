e7bda246058aa657ff4cdd42a3da2c212da911d2
Josh Chaiken <josh@jchaike.com>
2013-08-13 15:30:48 -0400

    Code cleanup
    Removed/modified some comments

--- instagram.php
+++ instagram.php
@@ -1,15 +1,11 @@
 <?php
 include ('parameters.php');
 
-/*******************retrieve user ID***************************/
-
+	//retrieve user ID
     $url = "https://api.instagram.com/v1/users/search?q=" . $userName . "&access_token=" . $accesstoken;
 	$content = file_get_contents($url);
 	$result = json_decode($content, true);
 	$userID = $result['data'][0]['id'];
-
-/******************end retrieve user ID**********************/
-
 
 $URL="https://api.instagram.com/v1/users/" . $userID . "/media/recent?access_token=" . $accesstoken . "&count=" . $count;
 
@@ -18,12 +14,11 @@ $json = json_decode($content, true);
 
 foreach($json['data'] as $item) {
 
-/*style*/
+	//Edit the styling here
     print '<img src="' . $item['images'][$imageType]['url'] . '" width="25%" height="auto" border="0" alt="" >';
-/*end styling*/
-
 }
 ?>
+
 <!--
 "Simpstagram" PHP Easy Script created by Josh Chaiken (jchaike)
 jchaike.com
